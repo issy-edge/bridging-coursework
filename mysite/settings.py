d441be9030aba059fb1123e10e93b31ce16395ec
issye <issy.edge@hotmail.co.uk>
2020-05-25 19:08:19 +0100

    Not even sure

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 #STATICFILES_DIRS = [
  #   os.path.join(BASE_DIR, 'static')
 #]
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 # MEDIA_URL = '/media/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
